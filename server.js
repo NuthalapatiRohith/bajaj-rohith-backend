d681d07c790cdb455eee02b9ca851291f94368ff
NuthalapatiRohith <103188449+NuthalapatiRohith@users.noreply.github.com>
2024-09-22 19:20:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 
 const allowedOrigins = [
-  'https://bajaj-rohith-frontend-fb2704ec03b9.herokuapp.com',
+  'https://bajaj-keerthi-frontend-bfd99e58f298.herokuapp.com',
   'http://localhost:3000',
   'http://localhost:3001'
 ];
@@ -22,10 +22,10 @@ app.use(cors({
     }
 }));
 
-const fullName = 'NUTHALAPATI ROHITH';
-const dob = '31052004';
-const email = 'nn8903@srmist.edu.in';
-const rollNumber = 'RA2111003010863';
+const fullName = 'KEERTHI GURUGUBELLI';
+const dob = '15072004';
+const email = 'gg0692@srmist.edu.in';
+const rollNumber = 'RA2111030010093';
 
 function separateData(data) {
     const numbers = [];
